b7524c706e077d0cc6801fbbe806c8101371b55c
曹艳玲 <caoyanling@caoyanlingdeMacBook-Pro.local>
2020-05-12 13:59:04 +0800

    动态数据

--- index.js
+++ index.js
@@ -46,25 +46,17 @@ module.exports = Event.extend(function Base(container, config) {
     var cfg = this.mergeConfig(config);
     //如果有需要的话,更新样式
 
-    var html = `
-    <div id="certify" style="height:100%;">
-      <div class="swiper-container">
-        <div class="swiper-wrapper">
-          <div class="swiper-slide">Slide 1</div>
-          <div class="swiper-slide">Slide 2</div>
-          <div class="swiper-slide">Slide 3</div>
-          <div class="swiper-slide">Slide 4</div>
-          <div class="swiper-slide">Slide 5</div>
-          <div class="swiper-slide">Slide 6</div>
-          <div class="swiper-slide">Slide 7</div>
-          <div class="swiper-slide">Slide 8</div>
-          <div class="swiper-slide">Slide 9</div>
-          <div class="swiper-slide">Slide 10</div>
-        </div>
-      <!-- Add Pagination -->
-      <div class="swiper-pagination"></div>
-    </div>
-    </div>`
+    var html = `<div id="certify" style="height:100%;"><div class="swiper-container"><div class="swiper-wrapper">`
+      for(var i =0;i<data.length;i++){
+        html+=`<div class="swiper-slide">`
+        html+= `<img class="eventCover" src="${data[i]["eventCover"]}" />`
+        html+= `<p class="eventName">${data[i]["eventName"]}</p>`
+        html+= `<p class="eventTime">${data[i]["eventTime"]}</p>`
+        html+=`<span class="relatedDepartmentCount">${data[i]["relatedDepartmentCount"]}</span>`
+        html+=`</div>`
+      }
+    html+= `</div></div></div>`
+       
 
     this.container.html(html);
 
